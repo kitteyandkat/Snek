e224cb0bad31c67d4bbd55bbf83aa2438b1065c7
Sarah Johnson <kitteyandkat@aim.com>
2023-01-07 14:20:12 -0500

    repo updates

--- snek.js
+++ snek.js
@@ -1,3 +1,10 @@
 window.onload = function() {
     document.getElementById("themeSong").play();
 }
+
+let startScreen = document.getElementById('.startScreen');
+let startButton = document.getElementsByClassName('btn')
+
+startButton.onclick = function () {
+  startScreen.classList.toggle('fade');
+}
